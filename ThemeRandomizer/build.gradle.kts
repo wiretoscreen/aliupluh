baa571ed45e78b76b0498e87f965de4fe9cb4c4a
wiretoscreen <75181938+wiretoscreen@users.noreply.github.com>
2025-10-25 05:43:59 -0300

    fix(ThemeRandomizer): Adds error messages & Fix install

--- ThemeRandomizer/build.gradle.kts
+++ ThemeRandomizer/build.gradle.kts
@@ -1,9 +1,10 @@
-version = "1.0.0"
+version = "1.0.1"
 description = "Randomizes your theme every Aliucord launch"
 
 aliucord {
     changelog.set("""
+        - **1.0.1** • Fix install & Adds error messages
         - **1.0.0** • Initial Release
     """.trimIndent())
-    excludeFromUpdaterJson.set(true)
+    excludeFromUpdaterJson.set(false)
 }
--- ThemeRandomizer/src/main/kotlin/com/wiretoscreen/plugins/ThemeRandomizer.kt
+++ ThemeRandomizer/src/main/kotlin/com/wiretoscreen/plugins/ThemeRandomizer.kt
@@ -1,19 +1,47 @@
 package com.wiretoscreen.plugins
 
 import android.content.Context
+import android.os.Handler
+import android.os.Looper
 import com.aliucord.Utils
 import com.aliucord.annotations.AliucordPlugin
 import com.aliucord.entities.Plugin
 import com.discord.api.commands.ApplicationCommandType
+import com.discord.stores.StoreStream
 import com.aliucord.api.SettingsAPI
+import com.aliucord.api.NotificationsAPI
+import com.aliucord.entities.NotificationData
 import com.aliucord.PluginManager
 
+fun showNotification(title: String, body: String) {
+    Handler(Looper.getMainLooper()).postDelayed({ // If not delayed, notification won't show and I'm too stupid to figure out a better way to do it.
+        val notification = NotificationData() // https://aliucord.github.io/dokka/html/-aliucord/com.aliucord.entities/-notification-data/index.html
+        notification.title = title
+        notification.body = body
+        notification.autoDismissPeriodSecs = 30
+        notification.onClick = {
+            StoreStream.getMessagesLoader().jumpToMessage(811275162715553823L, 846165924708614194L) // #plugins-list - Themer
+        }
+
+        NotificationsAPI.display(notification)
+    }, 10000)
+}
+
 fun randomizeTheme() {
-    val plugin = PluginManager.plugins["Themer"] ?: return
+    val plugin = PluginManager.plugins["Themer"]
+    
+    if (plugin == null){
+        showNotification("ThemeRandomizer", "Themer must be installed for this plugin to work")
+        return
+    }
+    
     val sets: SettingsAPI = plugin.settings
 
     val themeSwitch = sets.getAllKeys().filter { it.endsWith("-enabled") }
-    if (themeSwitch.isEmpty()) return
+    if (themeSwitch.isEmpty()) {
+        showNotification("ThemeRandomizer", "No themes found to randomize")
+        return
+    }
 
     val current = themeSwitch.find { sets.getBool(it, false) }
     current?.let { sets.setBool(it, false) }
