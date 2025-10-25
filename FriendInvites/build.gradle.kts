c5d979e9f2f2344920ff1058be087505b20a0d70
wiretoscreen <75181938+wiretoscreen@users.noreply.github.com>
2025-10-25 14:38:18 -0300

    feat(FriendInvites): Release friendinvites plugin

---
+++ FriendInvites/build.gradle.kts
@@ -0,0 +1,10 @@
+version = "1.0.0"
+description = "generate, list, and remove Discord friend invites."
+
+aliucord {
+    // Changelog of your plugin
+    changelog.set("""
+        * **1.0.0**: Initial Release
+    """.trimIndent())
+    excludeFromUpdaterJson.set(false)
+}
