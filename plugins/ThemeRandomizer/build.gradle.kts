0da88b45213e3cb10de06f6a65696959f32f32dd
wire <75181938+wiretoscreen@users.noreply.github.com>
2026-09-15 15:34:59 -0300

    chore(ThemeRandomizer): Changelog changing

--- plugins/ThemeRandomizer/build.gradle.kts
+++ plugins/ThemeRandomizer/build.gradle.kts
@@ -1,10 +1,15 @@
-version = "1.0.1"
+version = "1.0.2"
+
 description = "Randomizes your theme every Aliucord launch"
 
 aliucord {
-    changelog.set("""
-        - **1.0.1** • Fix install & Adds error messages
-        - **1.0.0** • Initial Release
-    """.trimIndent())
+    changelog.set(
+        """
+        * **1.0.2** • exclude current theme from randomization, use toasts and improve error messages
+        * **1.0.1** • Fix install & Adds error messages
+        * **1.0.0** • Initial Release
+        """
+            .trimIndent()
+    )
     deploy.set(true)
 }
